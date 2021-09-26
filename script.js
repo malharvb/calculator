995b906a08d109853fc61008ab54d65d9f50239f
malhar <malharkajale@gmail.com>
2021-09-26 17:24:52 +0530

    removed Del button

--- script.js
+++ script.js
@@ -104,6 +104,7 @@ function popDisp(e)
         c3 = 0;
         operated_now = 0;
         
+        
 
         
         //console.log(dispCont +' ' + op)
@@ -146,7 +147,7 @@ function popDisp(e)
     //this is for when after entering 2 numbers another operator is clicked
     if(no != '' && dispCont.length == 1 && e.target.className == 'operators')
     {
-
+        
         dispCont.push(no);
         no = '';
         c3 == 0;
@@ -163,56 +164,7 @@ function popDisp(e)
         dot.disabled = true;
     } 
 
-    if(e.target.className == 'back')
-    {   
-        if(disp.textContent != '')
-        {
-            d = disp.textContent;
-        
-            s = d.slice(-1);
-            disp.textContent = d.slice(0,-1)
 
-            tp = [];
-            //regex to break a string when it encounter +,-,/,*
-            tp = disp.textContent.split(/[\+\-\u00f7\u00d7]/);
-            console.log(tp);
-            if(tp.length == 2)
-            {
-                tp.pop();
-            }
-            if(tp[0] != '')
-            {
-                dispCont[0] = tp[0];
-            }
-            else
-            {
-                no = ''
-                dispCont.length = 0
-            }
-            if(dispCont.length == 1)
-            {
-                no = '';
-            }
-            console.log(dispCont,no)
-            if(s != '' )
-            {
-                if(s == '\u00f7' || s == '\u00d7' || s == '+' || s == '-')
-                {
-                    c2 = 0;
-                    op = ''
-                    no = ''
-                }
-                else if(s == '.')
-                {
-                    dot.disabled = false;
-                }
-                else
-                {
-                    no = no.slice(0,-1)
-                }
-            }
-        }
-    }
             
         
     
